f2c93ee72b12e0e4edb707bd2607124fe728284c
Vanshika Parashar <parasharvanshika2005@gmail.com>
2025-10-16 08:43:32 +0530

    Time: 5 ms (52.82%), Space: 45.2 MB (50.82%) - LeetHub

---
+++ 0006-zigzag-conversion/0006-zigzag-conversion.java
@@ -0,0 +1,33 @@
+class Solution {
+    public String convert(String s, int k) {
+        if(k==1){
+            return s;
+        }
+        
+        StringBuilder[] rows=new StringBuilder[k];
+        for(int i=0;i<k;i++){
+            rows[i]=new StringBuilder();
+        }
+        int currrow=0;
+        boolean grow=false;
+        for(char c:s.toCharArray()){
+            rows[currrow].append(c);
+            if(currrow==0||currrow==k-1){
+                grow=!grow;
+            }
+            if(grow==true){
+                currrow+=1;
+
+            }
+            else{
+                currrow-=1;
+            }
+        }
+        StringBuilder ans=new StringBuilder();
+        for(StringBuilder r:rows){
+            ans.append(r);
+        }
+        return (ans.toString());
+        
+    }
+}
