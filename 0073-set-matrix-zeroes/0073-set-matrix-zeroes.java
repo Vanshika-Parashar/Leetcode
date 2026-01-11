1521d2f0490b204651e75ee50944aad9a9c910e5
Vanshika Parashar <parasharvanshika2005@gmail.com>
2026-01-11 23:33:50 +0530

    Time: 1 ms (59.04%), Space: 47.7 MB (7.54%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,25 +1,28 @@
 class Solution {
     public void setZeroes(int[][] a) {
-        int m=a[0].length;
-        int n=a.length;
-        int []row=new int[n];
-        int []col=new int[m];
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(a[i][j]==0){
-                    row[i]=1;
-                    col[j]=1;
+        int m=a.length;
+        int n=a[0].length;
+        int [][] help=new int[m][n];
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                help[i][j]=a[i][j];
+            }
+        }
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(help[i][j]==0){
+                    for(int k=0;k<m;k++){
+                        a[k][j]=0;
+                    }
+                    for(int k=0;k<n;k++){
+                        a[i][k]=0;
+                    }
+                }
+            }
+        }
+        
+        
 
-                }
-            }
-        }
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(row[i]==1||col[j]==1){
-                    a[i][j]=0;
-                }
-            }
-        }
         
     }
 }
