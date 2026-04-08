6efe4e5826c31a929ec873865f586d89da5e3be9
Vanshika Parashar <parasharvanshika2005@gmail.com>
2026-04-08 11:20:37 +0530

    Time: 3 ms (41.62%), Space: 46.6 MB (52.5%) - LeetHub

--- 0051-n-queens/0051-n-queens.java
+++ 0051-n-queens/0051-n-queens.java
@@ -1,88 +1,171 @@
 class Solution {
     public List<List<String>> solveNQueens(int n) {
-        char[][]board=new char[n][n];
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                board[i][j]='.';
-            }
-        }
-        List<List<String>>ans=new ArrayList<>();
-        nqueen(board,n,0,ans);
-        return ans;
+    //     char[][]board=new char[n][n];
+    //     for(int i=0;i<n;i++){
+    //         for(int j=0;j<n;j++){
+    //             board[i][j]='.';
+    //         }
+    //     }
+    //     List<List<String>>ans=new ArrayList<>();
+    //     nqueen(board,n,0,ans);
+    //     return ans;
         
-    }
-    public void nqueen(char[][]board,int n,int row,List<List<String>>ans){
-        if(row==n){
-           List<String> list = new ArrayList<>();
-            for(int i=0;i<n;i++){
-                list.add(new String(board[i]));
-            }
-            ans.add(list);
-            return;
-        }
-        for(int j=0;j<n;j++){
-            if(issafe(board,row,j)){
-                board[row][j]='Q';
-                nqueen(board,n,row+1,ans);
-                board[row][j]='.';
-            }
+    // }
+    // public void nqueen(char[][]board,int n,int row,List<List<String>>ans){
+    //     if(row==n){
+    //        List<String> list = new ArrayList<>();
+    //         for(int i=0;i<n;i++){
+    //             list.add(new String(board[i]));
+    //         }
+    //         ans.add(list);
+    //         return;
+    //     }
+    //     for(int j=0;j<n;j++){
+    //         if(issafe(board,row,j)){
+    //             board[row][j]='Q';
+    //             nqueen(board,n,row+1,ans);
+    //             board[row][j]='.';
+    //         }
             
 
 
+    //     }
+    // }
+    // public boolean issafe(char[][]board,int row,int col){
+    //     int i,j;
+    //     int n=board.length;
+    //     //column check
+    //     i=row;
+    //     j=col;
+    //     while(j<n){
+    //         if(board[i][j]=='Q')return false;
+    //         j++;
+    //     }
+    //     //row check
+    //     i=0;
+    //     j=col;
+    //     while(i<n){
+    //         if(board[i][j]=='Q')return false;
+    //         i++;
+    //     }
+    //     //north-east check
+    //     i=row;
+    //     j=col;
+    //     while(i>=0 && j<n){
+    //         if(board[i][j]=='Q')return false;
+    //         i--;
+    //         j++;
+    //     }
+    //     //south-east check
+    //     i=row;
+    //     j=col;
+    //     while(i<n&& j<n){
+    //         if(board[i][j]=='Q')return false;
+    //         i++;
+    //         j++;
+    //     }
+    //     //south-west check
+    //     i=row;
+    //     j=col;
+    //     while(i<n && j>=0){
+    //         if(board[i][j]=='Q')return false;
+    //         i++;
+    //         j--;
+    //     }
+    //     //north-west check
+    //     i=row;
+    //     j=col;
+    //     while(i>=0 && j>=0){
+    //         if(board[i][j]=='Q')return false;
+    //         i--;
+    //         j--;
+    //     }
+    //     return true;
+
+        
+        char[][]ch=new char[n][n];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                ch[i][j]='.';
+            }
         }
+        List<List<String>>ans=new ArrayList<>();
+        List<String>list=new ArrayList<>();
+        return nqueen(ch,n,0,ans);
     }
-    public boolean issafe(char[][]board,int row,int col){
+    public List<List<String>> nqueen(char[][]ch,int n,int row,List<List<String>>ans){
+        if(row==n){
+            List<String>list=new ArrayList<>();
+            for(int i=0;i<n;i++){
+                list.add(new String(ch[i]));
+            }
+            ans.add(list);
+            return ans;
+        }
+        for(int j=0;j<n;j++){
+            if(ispossible(ch,row,j,n)){
+            ch[row][j]='Q';
+            nqueen(ch,n,row+1,ans);
+            ch[row][j]='.';
+            }
+        }
+        return ans;
+        
+    }
+    public boolean ispossible(char[][]ch,int row,int col,int n){
         int i,j;
-        int n=board.length;
-        //row check
+        //down
         i=row;
         j=col;
-        while(j<n){
-            if(board[i][j]=='Q')return false;
-            j++;
+        while(i<n){
+            if(ch[i][j]=='Q')return false;
+            i++;
         }
-        //column check
+        //column(hoda smj nhi aaya )
         i=0;
         j=col;
         while(i<n){
-            if(board[i][j]=='Q')return false;
+            if(ch[i][j]=='Q')return false;
             i++;
         }
-        //north-east check
-        i=row;
-        j=col;
-        while(i>=0 && j<n){
-            if(board[i][j]=='Q')return false;
-            i--;
-            j++;
-        }
-        //south-east check
-        i=row;
-        j=col;
-        while(i<n&& j<n){
-            if(board[i][j]=='Q')return false;
-            i++;
-            j++;
-        }
-        //south-west check
-        i=row;
-        j=col;
-        while(i<n && j>=0){
-            if(board[i][j]=='Q')return false;
-            i++;
-            j--;
-        }
-        //north-west check
-        i=row;
-        j=col;
-        while(i>=0 && j>=0){
-            if(board[i][j]=='Q')return false;
-            i--;
-            j--;
-        }
-        return true;
+       //north-east
+       i=row;
+       j=col;
+       while(i>=0 && j<n){
+        if(ch[i][j]=='Q')return false;
+        i--;
+        j++;
+       }
+    //    south-east
+       i=row;
+       j=col;
+       while(i<n && j<n){
+        if(ch[i][j]=='Q')return false;
+        i++;
+        j++;
+       }
+    //    north-west
+       i=row;
+       j=col;
+       while(i>=0 && j>=0){
+        if(ch[i][j]=='Q')return false;
+        i--;
+        j--;
+       }
+       //south-west
+       i=row;
+       j=col;
+       while(i<n && j>=0){
+        if(ch[i][j]=='Q')return false;
+        i++;
+        j--;
+       }
+       return true;
+
 
         
+
+
     }
 
 }
