a024e6edb1a38106ac474672f5556e2c2ed97a32
Vanshika Parashar <parasharvanshika2005@gmail.com>
2026-04-11 18:40:25 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -11,29 +11,30 @@ class Solution {
             return ans;
         }
          //approach 1 checking false;
-        for(int i=0;i<nums.length;i++){
-            if(check[i]==false){
-                list.add(nums[i]);
-                check[i]=true;
-                find(nums,list,ans,check);
-                check[i]=false;
-                list.remove(list.size()-1);
-            }
-        }
-        
-        // for (int i = 0; i < nums.length; i++) {
-        //     if (check[i]) continue;
-            
-        //     check[i] = true;
-        //     list.add(nums[i]);
-            
-        //     find(nums, list, ans, check);
-            
-        //     // backtrack
-        //     list.remove(list.size() - 1);
-        //     check[i] = false;
-
+        // for(int i=0;i<nums.length;i++){
+        //     if(check[i]==false){
+        //         list.add(nums[i]);
+        //         check[i]=true;
+        //         find(nums,list,ans,check);
+        //         check[i]=false;
+        //         list.remove(list.size()-1);
+        //     }
         // }
+        //more optimised
+        
+        for (int i = 0; i < nums.length; i++) {
+            if (check[i]) continue;
+            
+            check[i] = true;
+            list.add(nums[i]);
+            
+            find(nums, list, ans, check);
+            
+            // backtrack
+            list.remove(list.size() - 1);
+            check[i] = false;
+
+        }
         return ans;
     }
     
