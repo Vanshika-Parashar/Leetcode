7a8a839d45f6ebc416edc36c1976188609ca2aa1
Vanshika Parashar <parasharvanshika2005@gmail.com>
2026-04-22 12:22:06 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,49 +1,18 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        int n=nums.length/2;
-        int max=nums[0];
-        Arrays.sort(nums);
-        int sum=1;
-        for(int i=1;i<nums.length;i++){
-            
-            if(nums[i]==nums[i-1]){
-                sum++;
-            }
-            else{
-                sum=1;
-                max=nums[i];
-
+    int freq=0;
+    int ans=nums[0];
+    for(int i=0;i<nums.length;i++){
+        if(freq==0){
+            ans=nums[i];
         }
-        if(sum>n){
-            return nums[i];
+        if(freq==nums[i]){
+            freq++;
         }
-        
-
-        
+        else{
+            freq--;
+        }
     }
-    return max;
-    // HashMap<Integer,Integer>h=new HashMap<>();
-    // Set<Integer>s=new HashSet<>();
-    // for(int i:nums){
-    //     s.add(i);
-    // }
-    // for(int i:nums){
-    //     if(h.containsKey(i)){
-    //         h.put(i,h.get(i)+1);
-    //     }
-    //     else{
-    //         h.put(i,1);
-    //     }
-    // }
-    // int max=0;
-    // for(int i:s){
-    //     if(h.get(i)>n){
-    //         return i;
-    //     }
-        
-
-    // }
-    // return -1;
-
+    return ans;
 }
 }
