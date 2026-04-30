53a0be3f2635e42793535f3c823569254bafd198
Vanshika Parashar <parasharvanshika2005@gmail.com>
2026-04-30 12:57:48 +0530

    Time: 2 ms (95.24%), Space: 45.5 MB (91.18%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,45 +1,24 @@
 class Solution {
     public List<List<Integer>> combinationSum(int[] arr, int t) {
-        // List<List<Integer>>l1=new ArrayList<>();
-        // List<Integer>l2=new ArrayList<>();
-        // return combination(arr, t,l1,l2,0);
-        List<Integer>ans=new ArrayList<>();
-        List<List<Integer>>finalans=new ArrayList<>();
-        return combination(arr,t,ans,finalans,0,0);
-        
+        List<List<Integer>>ans=new ArrayList<>();
+        List<Integer>list=new ArrayList<>();
+        return find(arr,0,list,ans,t);
     }
-    public List<List<Integer>>combination(int[]arr,int t,List<Integer>ans,List<List<Integer>>finalans,int sum,int idx){
-        if(sum==t){
-            List<Integer>list=new ArrayList<>();
-            for(int i=0;i<ans.size();i++){
-                list.add(ans.get(i));
-            }
-            finalans.add(list);
-            return finalans;
+    public List<List<Integer>> find(int[]arr,int idx,List<Integer>list,List<List<Integer>>ans,int t){
+        if(t==0){
+            ans.add(new ArrayList<>(list));
+            return ans;
         }
         for(int i=idx;i<arr.length;i++){
-            sum+=arr[i];
-            ans.add(arr[i]); 
-            if(sum<=t){
-                combination(arr,t,ans,finalans,sum,i);
-                
+            if(t-arr[i]>=0){
+                list.add(arr[i]);
+                find(arr,i,list,ans,t-arr[i]);
+                list.remove(list.size()-1);
+
             }
-            ans.remove(ans.size()-1);
-            sum-=arr[i];
+            
+            
         }
-        return finalans;
+        return ans;
     }
-    // public static List<List<Integer>> combination(int[]arr,int t,List<List<Integer>>l1,List<Integer>l2,int idx){
-    //     if(t==0){
-    //         l1.add(new ArrayList(l2));
-    //         return l1;
-    //     }
-    //     for(int i=idx;i<arr.length;i++){
-    //         if(t>=arr[i]){
-    //             l2.add(arr[i]);
-    //             combination(arr,t-arr[i],l1,l2,i);
-    //             l2.remove(l2.size()-1);
-    //         }
-    //     }
-    //     return l1;
-    }
+}
