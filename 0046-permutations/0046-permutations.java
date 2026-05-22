0afacc9ffc1559f204556968394dc1089e3e80bd
Vanshika Parashar <parasharvanshika2005@gmail.com>
2026-05-22 22:40:39 +0530

    Time: 1 ms (87.1%), Space: 45.3 MB (78.1%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -3,39 +3,26 @@ class Solution {
        List<List<Integer>>ans=new ArrayList<>();
        List<Integer>list=new ArrayList<>();
        boolean[]check=new boolean[nums.length];
-       return find(nums,list,ans,check);
+       return find(nums,check,ans,list);
     }
-    public List<List<Integer>> find(int[]nums,List<Integer>list,List<List<Integer>>ans,boolean[]check){
+
+    
+    public List<List<Integer>> find(int[]nums,boolean[]check,List<List<Integer>>ans, List<Integer>list){
         if(list.size()==nums.length){
             ans.add(new ArrayList<>(list));
             return ans;
         }
-         //approach 1 checking false;
-        // for(int i=0;i<nums.length;i++){
-        //     if(check[i]==false){
-        //         list.add(nums[i]);
-        //         check[i]=true;
-        //         find(nums,list,ans,check);
-        //         check[i]=false;
-        //         list.remove(list.size()-1);
-        //     }
-        // }
-        //more optimised
-        
-        for (int i = 0; i < nums.length; i++) {
-            if (check[i]) continue;
-            
-            check[i] = true;
+        for(int i=0;i<nums.length;i++){
+            if(check[i]==true)continue;
             list.add(nums[i]);
-            
-            find(nums, list, ans, check);
-            
-            // backtrack
-            list.remove(list.size() - 1);
-            check[i] = false;
-
+            check[i]=true;
+            find(nums,check,ans,list);
+            list.remove(list.size()-1);
+            check[i]=false;
         }
+        
         return ans;
     }
     
+
 }
