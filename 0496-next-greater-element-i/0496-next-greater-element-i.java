5cca77323953c1fa4e1308c551f724c52314df50
Vanshika Parashar <parasharvanshika2005@gmail.com>
2026-05-29 19:54:36 +0530

    Time: 4 ms (75.77%), Space: 45.2 MB (61.14%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,29 +1,24 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        List<Integer>n=new ArrayList<>();
-        for(int i=0;i<nums2.length;i++){
-            n.add(nums2[i]);
+        HashMap<Integer,Integer>n=new HashMap<>();
+        Stack<Integer>st=new Stack<>();
+        int l2=nums2.length;
+        for(int i=l2-1;i>=0;i--){
+            while(!st.isEmpty()&& st.peek()<=nums2[i]){
 
+                st.pop();
+            }
+            if(st.isEmpty()){
+                n.put(nums2[i],-1);
+            }
+            else{
+                n.put(nums2[i],st.peek());
+            }
+            st.push(nums2[i]);
         }
         int[]ans=new int[nums1.length];
         for(int i=0;i<nums1.length;i++){
-            ans[i]=-1;
-        }
-        for(int i=0;i<nums1.length;i++){
-            int index=n.indexOf(nums1[i]);
-            for(int j=index+1;j<n.size();j++){
-                if(nums1[i]<n.get(j)){
-                    ans[i]=n.get(j);
-                    break;
-                }
-                
-            }
-        }
-        for(int i=0;i<ans.length;i++){
-            if(ans[i]==nums1[i]){
-                ans[i]=-1;
-            }
-            
+            ans[i]=n.get(nums1[i]);
         }
         return ans;
     }
