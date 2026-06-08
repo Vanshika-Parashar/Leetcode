e82ab95a9f3be00df58be567fe08d49b619d9f1b
Vanshika Parashar <parasharvanshika2005@gmail.com>
2026-06-09 00:17:31 +0530

    Time: 18 ms (81.28%), Space: 97.8 MB (81.63%) - LeetHub

--- 0077-combinations/0077-combinations.java
+++ 0077-combinations/0077-combinations.java
@@ -1,24 +1,22 @@
 class Solution {
     public List<List<Integer>> combine(int n, int k) {
-   
-        int[]nums=new int[n];
+        int[]arr=new int[n];
         for(int i=0;i<n;i++){
-            nums[i]=i+1;
+            arr[i]=i+1;
         }
         List<List<Integer>>ans=new ArrayList<>();
-        List<Integer>list=new ArrayList<>();
-        return find(nums,k,0,0,ans,list);
+        List<Integer>l=new ArrayList<>();
+        return find(arr,0,k,ans,l);
     }
-
-    public List<List<Integer>> find(int[]nums,int k,int idx,int count,List<List<Integer>>ans,List<Integer>list){
-        if(count==k){
-            ans.add(new ArrayList<>(list));
+    public List<List<Integer>> find(int[]arr,int idx,int k,List<List<Integer>>ans,List<Integer>l){
+        if(k==0){
+            ans.add(new ArrayList<>(l));
             return ans;
         }
-        for(int i=idx;i<nums.length;i++){
-            list.add(nums[i]);
-            find(nums,k,i+1,count+1,ans,list);
-            list.remove(list.size()-1);
+        for(int i=idx;i<arr.length;i++){
+            l.add(arr[i]);
+            find(arr,i+1,k-1,ans,l);
+            l.remove(l.size()-1);
         }
         return ans;
     }
