3d5a6541ae72bffea27d269b9fff74f301519199
Vanshika Parashar <parasharvanshika2005@gmail.com>
2026-06-08 20:18:03 +0530

    Time: 1 ms (81.68%), Space: 42.4 MB (66.43%) - LeetHub

--- 0202-happy-number/0202-happy-number.java
+++ 0202-happy-number/0202-happy-number.java
@@ -1,56 +1,29 @@
-// class Solution {
-//     public boolean isHappy(int n) {
-         
-//          Set<Integer>s=new HashSet<>();
-//          return happy(n,s);
-         
-         
-//     }
-//     public boolean happy(int n,Set<Integer>s){
-//         if(n==1){
-//             return true;
-//         }
-//         String s1=String.valueOf(n);
-//         if(s.contains(n)){
-//             return false;
-//         }
-//         int sum=0;
-//         for(int i=0;i<s1.length();i++){
-//             int sq=(s1.charAt(i)-'0')*(s1.charAt(i)-'0');
-//             sum+=sq;
-//         }
-//         s.add(n);
-//         return happy(sum,s);
-//     }
-    
-    
-// }
 class Solution {
     public boolean isHappy(int n) {
-         Set<Integer>n1=new HashSet<>();
-        return happy(n,n1);
+         Set<Integer>s=new HashSet<>();
+         return find(n,s);
         
-    
-    
+        
     }
-    public static Boolean happy(int n,Set<Integer>n1){
+    public boolean find(int n,Set<Integer>s){
         if(n==1){
             return true;
         }
-       
-        String s=Integer.toString(n);
-        char[]ch=s.toCharArray();
-        if(n1.contains(n)){
+        if(s.contains(n)){
             return false;
         }
         int sum=0;
-        for(int i=0;i<ch.length;i++){
-            char c=ch[i];
-            sum+=Character.getNumericValue(c)*Character.getNumericValue(c);
+        int x=n;
+        int last=0;
+        while(x!=0){
+            last=x%10;
+            sum+=last*last;
+            x=x/10;
+
         }
-        n1.add(n);
-        return happy(sum,n1);
-        
+        s.add(n);
+        return find(sum,s);
+
     }
     
     
