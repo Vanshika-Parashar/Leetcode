258e6b63b252c5a5976ca4c5e5c4d6f35abadc7e
Vanshika Parashar <parasharvanshika2005@gmail.com>
2026-06-08 19:50:49 +0530

    Time: 2 ms (17.06%), Space: 42.3 MB (66.43%) - LeetHub

--- 0202-happy-number/0202-happy-number.java
+++ 0202-happy-number/0202-happy-number.java
@@ -1,48 +1,38 @@
 class Solution {
     public boolean isHappy(int n) {
-    //      Set<Integer>n1=new HashSet<>();
-    //     return happy(n,n1);
-        HashSet<Integer>n1=new HashSet<>();
-        return happy(n,n1);
-    }
-    public static Boolean happy(int n,Set<Integer>n1){
-        if(n==1){
-            return true;
-        }
-        if(n1.contains(n)){
+         if(n<=0){
             return false;
-        }
-        String s=Integer.toString(n);
-        char[]ch=s.toCharArray();
-        int sum=0;
-        for(int i=0;i<ch.length;i++){
-            char c=ch[i];
-            sum+=Character.getNumericValue(c)*Character.getNumericValue(c);
-        }
-        n1.add(n);
-        return happy(sum,n1);
-
-    
-    
-    // }
-    // public static Boolean happy(int n,Set<Integer>n1){
-    //     if(n==1){
-    //         return true;
-    //     }
-       
-    //     String s=Integer.toString(n);
-    //     char[]ch=s.toCharArray();
-    //     if(n1.contains(n)){
-    //         return false;
-    //     }
-    //     int sum=0;
-    //     for(int i=0;i<ch.length;i++){
-    //         char c=ch[i];
-    //         sum+=Character.getNumericValue(c)*Character.getNumericValue(c);
-    //     }
-    //     n1.add(n);
-    //     return happy(sum,n1);
+         }
+         Set<Integer>s=new HashSet<>();
+         s.add(n);
+         int sum=find(n);
+         
+         while(sum>1){
+            if(s.contains(sum)){
+                return false;
+            }
+            else{
+                s.add(sum);
+                sum=find(sum);
+            }
+            
+         }
+         if(sum==1){
+            return true;
+         }
+         return false;
         
+    }
+    public int find(int n){
+        int sum=0;
+        int p=0;
+        while(n!=0){
+            p=n%10;
+            int sq=p*p;
+            sum+=sq;
+            n=n/10;
+        }
+        return sum;
     }
     
     
