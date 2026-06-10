eb20ff7d8e030a468afa7df8285364a9cb10669d
Vanshika Parashar <parasharvanshika2005@gmail.com>
2026-06-11 00:53:56 +0530

    Time: 6 ms (71.98%), Space: 45.5 MB (26.59%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.java
+++ 0040-combination-sum-ii/0040-combination-sum-ii.java
@@ -1,42 +1,24 @@
 class Solution {
     public List<List<Integer>> combinationSum2(int[] arr, int t) {
-        List<List<Integer>>l1=new ArrayList<>();
-        List<Integer>l2=new ArrayList<>();
+        List<List<Integer>>ans=new ArrayList<>();
+        List<Integer>l=new ArrayList<>();
         Arrays.sort(arr);
-        return combination(arr,t,l1,l2,0);
-        
+        return find(arr,t,0,ans,l);
+       
     }
-    public static List<List<Integer>> combination(int[]arr,int t,List<List<Integer>>l1, List<Integer>l2,int idx){
-        // if(t==0){
-        //     l1.add(new ArrayList<>(l2));
-        //     return l1;
-        // }
-        // for(int i=idx;i<arr.length;i++){
-        //     if (i > idx && arr[i] == arr[i - 1]) continue;
-
-        //     if(arr[i]<=t){
-        //         l2.add(arr[i]);
-        //         combination(arr,t-arr[i],l1,l2,i+1);
-        //         l2.remove(l2.size()-1);
-        //     }
-        // }
-        // return l1;
+    public List<List<Integer>> find(int []arr, int t,int idx,List<List<Integer>>ans,List<Integer>l){
         if(t==0){
-            List<Integer>l=new ArrayList<>();
-            for(int i=0;i<l2.size();i++){
-                l.add(l2.get(i));
-            }
-            l1.add(l);
-            return l1;
+            ans.add(new ArrayList<>(l));
+            return ans;
         }
         for(int i=idx;i<arr.length;i++){
             if(i>idx && arr[i]==arr[i-1])continue;
-            if(arr[i]<=t){
-                l2.add(arr[i]);
-                combination(arr,t-arr[i],l1,l2,i+1);
-                l2.remove(l2.size()-1);
+            if(t-arr[i]>=0){
+                l.add(arr[i]);
+                find(arr,t-arr[i],i+1,ans,l);
+                l.remove(l.size()-1);
             }
         }
-        return l1;
+        return ans;
     }
 }
