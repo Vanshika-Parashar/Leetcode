cebf153d80100bab951754fdb12567411e86be21
Vanshika Parashar <parasharvanshika2005@gmail.com>
2026-06-11 00:21:27 +0530

    Time: 2 ms (95.02%), Space: 45.5 MB (81.35%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,23 +1,22 @@
 class Solution {
     public List<List<Integer>> combinationSum(int[] arr, int t) {
-        List<List<Integer>>ans=new ArrayList<>();
-        List<Integer>list=new ArrayList<>();
-        return find(arr,0,list,ans,t);
+        List<List<Integer>>ans =new ArrayList<>();
+        List<Integer>l=new ArrayList<>();
+        return find(arr,t,0,ans,l);
+
+        
     }
-    public List<List<Integer>> find(int[]arr,int idx,List<Integer>list,List<List<Integer>>ans,int t){
+    public List<List<Integer>> find(int[]arr, int t,int idx,List<List<Integer>>ans,List<Integer>l){
         if(t==0){
-            ans.add(new ArrayList<>(list));
+            ans.add(new ArrayList<>(l));
             return ans;
         }
         for(int i=idx;i<arr.length;i++){
             if(t-arr[i]>=0){
-                list.add(arr[i]);
-                find(arr,i,list,ans,t-arr[i]);
-                list.remove(list.size()-1);
-
+                l.add(arr[i]);
+                find(arr,t-arr[i],i,ans,l);
+                l.remove(l.size()-1);
             }
-            
-            
         }
         return ans;
     }
