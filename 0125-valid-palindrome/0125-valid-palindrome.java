fa70b4805e59498e67db8528467b2c4981dbd40c
Vanshika Parashar <parasharvanshika2005@gmail.com>
2026-06-11 10:43:11 +0530

    Time: 5 ms (41.02%), Space: 44.9 MB (45.24%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -4,17 +4,34 @@ class Solution {
             return true;
         }
         StringBuffer x=new StringBuffer();
+    //     for(int i=0;i<s.length();i++){
+    //         char ch=s.charAt(i);
+    //         if(Character.isLetterOrDigit(ch)){
+    //             x.append(Character.toLowerCase(ch));
+
+    //         }
+       
+    // }
+    //     String a=x.toString();
+    //     String c=new StringBuffer(a).reverse().toString();
+    //     return a.equals(c);
         for(int i=0;i<s.length();i++){
             char ch=s.charAt(i);
-            if(Character.isLetterOrDigit(ch)){
+            if(isaphanumeric(ch)){
                 x.append(Character.toLowerCase(ch));
-
             }
-       
-    }
+        }
         String a=x.toString();
-        String c=new StringBuffer(a).reverse().toString();
+        String c=new StringBuffer(x).reverse().toString();
         return a.equals(c);
+        
             
+
+}
+public boolean isaphanumeric(char ch){
+    if(ch>=48 && ch<=57)return true;
+    else if(ch>=65 && ch<=90)return true;
+    else if(ch>=97 && ch<=122)return true;
+    return false;
 }
 }
