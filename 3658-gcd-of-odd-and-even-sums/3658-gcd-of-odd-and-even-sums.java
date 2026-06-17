76e39b3b32212a30f9baa92adb940a211d99b047
Vanshika Parashar <parasharvanshika2005@gmail.com>
2026-06-17 12:59:36 +0530

    Time: 0 ms (100%), Space: 42.3 MB (87.23%) - LeetHub

--- 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.java
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.java
@@ -1,36 +1,37 @@
 class Solution {
     public int gcdOfOddEvenSums(int n) {
-        int oddsum=0;
-        int  evensum=0;
-        for(int i=1;i<=n*2;i++){
-            if(i%2==0){
-                evensum+=i;
-            }
-            else{
-                oddsum+=i;
-            }
-        }
+        return n;
+    //     int oddsum=0;
+    //     int  evensum=0;
+    //     for(int i=1;i<=n*2;i++){
+    //         if(i%2==0){
+    //             evensum+=i;
+    //         }
+    //         else{
+    //             oddsum+=i;
+    //         }
+    //     }
 
-        int ans=find(evensum,oddsum);
-        return ans;
+    //     int ans=find(evensum,oddsum);
+    //     return ans;
         
-    }
-    public int find(int a,int b){
-        if(a>b){
-            while(b!=0){
-                int temp=a%b;
-                a=b;
-                b=temp;
-            }
-        }
-        else{
-            while(b!=0){
-                int temp=a%b;
-                a=b;
-                b=temp;
-            }
-        }
-        return a;
+    // }
+    // public int find(int a,int b){
+    //     if(a>b){
+    //         while(b!=0){
+    //             int temp=a%b;
+    //             a=b;
+    //             b=temp;
+    //         }
+    //     }
+    //     else{
+    //         while(b!=0){
+    //             int temp=a%b;
+    //             a=b;
+    //             b=temp;
+    //         }
+    //     }
+    //     return a;
 
     }
 }
