ad3624e0aaad1efaa21a0803e0da129cfe150f6e
Vanshika Parashar <parasharvanshika2005@gmail.com>
2026-06-22 00:41:42 +0530

    Time: 1281 ms (8.99%), Space: 0B (100%) - LeetHub

--- 0577-employee-bonus/0577-employee-bonus.sql
+++ 0577-employee-bonus/0577-employee-bonus.sql
@@ -3,4 +3,5 @@ FROM Employee as e
 LEFT JOIN Bonus as b 
 ON e.empId=b.empId
 where b.bonus<1000 OR b.bonus IS NULL;
+-- KYUKI HMKO CHAHIYE TOH SARE EMPLOYEES BUT USME CONDITION --LGADI ESLYE LEFT JOIN 
 
