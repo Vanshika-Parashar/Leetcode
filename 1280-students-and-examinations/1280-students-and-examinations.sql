2d20d4aaf9a56bcd9d485978a960b414c24b0505
Vanshika Parashar <parasharvanshika2005@gmail.com>
2026-06-22 01:02:24 +0530

    Time: 964 ms (87.36%), Space: 0B (100%) - LeetHub

---
+++ 1280-students-and-examinations/1280-students-and-examinations.sql
@@ -0,0 +1,10 @@
+# Write your MySQL query statement below
+SELECT s.student_id,s.student_name,su.subject_name,count(e.student_id) AS attended_exams
+FROM Students AS s
+CROSS JOIN Subjects as su
+LEFT JOIN Examinations AS e
+ON s.student_id=e.student_id AND
+su.subject_name=e.subject_name
+GROUP BY s.student_id,s.student_name,su.subject_name
+ORDER BY s.student_id,su.subject_name;
+-- cross use kra kyuki sare combinations check krne the 
