870854335bdcef432168a4bf7b9bb1ffe2589046
Vanshika Parashar <parasharvanshika2005@gmail.com>
2026-06-28 16:07:18 +0530

    Time: 3 ms (96.8%), Space: 48.8 MB (17.71%) - LeetHub

--- 1052-grumpy-bookstore-owner/1052-grumpy-bookstore-owner.java
+++ 1052-grumpy-bookstore-owner/1052-grumpy-bookstore-owner.java
@@ -1,7 +1,7 @@
 class Solution {
     public int maxSatisfied(int[] customer, int[] grumpy, int minutes) {
-        int sum=0;
         int max=0;
+        int sum=0;
         int n=grumpy.length;
         for(int i=0;i<n;i++){
             if(grumpy[i]==0){
@@ -19,15 +19,18 @@ class Solution {
         while(j<n){
             if(grumpy[i-1]==1){
                 sum-=customer[i-1];
+                
             }
             if(grumpy[j]==1){
                 sum+=customer[j];
+                
             }
             max=Math.max(max,sum);
             i++;
             j++;
         }
         return max;
+        
 
     }
 }
