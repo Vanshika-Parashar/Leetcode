c956ba6da4509009982c662563e923ae930305a3
Vanshika Parashar <parasharvanshika2005@gmail.com>
2026-07-13 00:39:06 +0530

    Time: 11 ms (68.19%), Space: 50 MB (76.15%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.java
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.java
@@ -1,36 +1,38 @@
 class Solution {
     public int shipWithinDays(int[] w, int d) {
-    int max=Integer.MIN_VALUE;
     int sum=0;
+    int max=Integer.MIN_VALUE;
     for(int i:w){
+        max=Math.max(i,max);
         sum+=i;
-        max=Math.max(max,i);
     }
+    int ans=0;
     int st=max;
     int end=sum;
     while(st<=end){
         int mid=st+(end-st)/2;
-        int days=find(w,mid);
+        int days=find(mid,w);
         if(days<=d){
+            ans=mid;
             end=mid-1;
         }
         else{
             st=mid+1;
         }
-    }
-    return st; 
 
     }
-    public int find(int[]w,int mid){
+    return ans;
+    }
+    public int find(int capacity,int[]arr){
         int count=0;
         int days=1;
-        for(int i:w){
-            if(count+i<=mid){
-                count+=i;
-            }
-            else{
+        for(int i:arr){
+            if(count+i>capacity){
                 days++;
                 count=i;
+            }
+            else{
+                count+=i;
             }
         }
         return days;
