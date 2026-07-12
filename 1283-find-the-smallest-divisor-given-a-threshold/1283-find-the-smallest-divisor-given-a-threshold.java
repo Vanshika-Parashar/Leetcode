1fd2c123778256c2ed370f676d2124deec39e314
Vanshika Parashar <parasharvanshika2005@gmail.com>
2026-07-12 18:35:51 +0530

    Time: 11 ms (54.85%), Space: 51 MB (53.55%) - LeetHub

--- 1283-find-the-smallest-divisor-given-a-threshold/1283-find-the-smallest-divisor-given-a-threshold.java
+++ 1283-find-the-smallest-divisor-given-a-threshold/1283-find-the-smallest-divisor-given-a-threshold.java
@@ -1,49 +1,15 @@
 class Solution {
     public int smallestDivisor(int[] nums, int threshold) {
-    //     int max=Integer.MIN_VALUE;
-    //     int min=Integer.MAX_VALUE;
-    //     for(int i:nums){
-    //         if(i>max){
-    //             max=i;
-    //         }
-    //     }
-    //     int ans=-1;
-    //     int st=1;
-    //     int end=max;
-    //     while(st<=end){
-    //         int mid=st+(end-st)/2;
-    //         int n=divide(nums,mid);
-    //         if(n<=threshold){
-    //             ans=mid;
-    //             end=mid-1;
-    //         }
-    //         else{
-    //             st=mid+1;
-    //         }
-    //     }
-    //     return ans;
-        
-    // }
-    // public static int divide(int []arr,int mid){
-    //     int sum=0;
-    //     for(int i:arr){
-    //         sum+=(i+mid-1)/mid;//for getting ans in ceil without using ceil function
-    //     }
-    //     return sum;
-    
-    int min=1;
+    int st=1;
     int max=Integer.MIN_VALUE;
     for(int i:nums){
-        if(i>max){
-            max=i;
-        }
+        max=Math.max(max,i);
     }
-        int st=min;
-        int end=max;
-        int ans=0;
+    int ans=0;
+    int end=max;
     while(st<=end){
         int mid=st+(end-st)/2;
-        int sum=divide(nums,mid);
+        int sum=find(mid,nums);
         if(sum<=threshold){
             ans=mid;
             end=mid-1;
@@ -51,14 +17,18 @@ class Solution {
         else{
             st=mid+1;
         }
-       
-        }
-        return ans;
     }
-    public int divide(int[]nums,int divisor){
+    return ans;
+    }
+    public int find(int divisor,int[]nums){
         int sum=0;
         for(int i:nums){
-            sum+=(i+divisor-1)/divisor;
+            if(i%divisor==0){
+                sum+=i/divisor;
+            }
+            else{
+                sum+=i/divisor+1;
+            }
         }
         return sum;
     }
