a1fc385ff3372d7e90cbf96dc6ee3ed2401af20c
Vanshika Parashar <parasharvanshika2005@gmail.com>
2026-07-18 01:48:03 +0530

    Time: 2 ms (61.52%), Space: 50.8 MB (76.07%) - LeetHub

--- 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.java
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.java
@@ -4,8 +4,8 @@ class Solution {
         if(k<=0){
             int ans=0;
             int len=0;
-            for(int x:nums){
-                if(x==0){
+            for(int i:nums){
+                if(i==0){
                     len++;
                 }
                 else{
@@ -15,25 +15,25 @@ class Solution {
             }
             ans+=len*(len+1)/2;
             return ans;
+
         }
-        int a=0,i=0,j=0,b=0,k2=0,n=nums.length;
-        while(i<n && nums[i]==0)i++;
-        
+        int a=0,i=0,j=0,k2=0,b=0,n=nums.length;
+        while(i<n && nums[i]!=1){
+            i++;
+        }
         while(j<n && k2<k){
             if(nums[j]==1){
                 k2++;
             }
             j++;
         }
-        if(k2<k)return 0;
         j--;
+        if(k2<k)return 0;
         b=j+1;
-        while(b<n && nums[b]==0){
+        while(b<n && nums[b]!=1){
             b++;
         }
         b--;
-        
-        //sliding window
         int sum=0;
         while(b<n){
             sum+=(i-a+1)*(b-j+1);
@@ -48,8 +48,11 @@ class Solution {
                 b++;
             }
             b--;
+           
         }
         return sum;
+
+
        
     }
 
