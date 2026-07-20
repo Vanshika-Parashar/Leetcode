bea84f477d4b1de56638de84ad5905c534b71c7b
Vanshika Parashar <parasharvanshika2005@gmail.com>
2026-07-20 11:08:42 +0530

    Time: 15 ms (60.64%), Space: 51.4 MB (74.79%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.java
+++ 0455-assign-cookies/0455-assign-cookies.java
@@ -13,7 +13,7 @@ class Solution {
                 i++;
                 j++;
             }
-            else if(g[i]>s[i])j++;
+            else if(g[i]>s[j])j++;
         }
        
        return count;
