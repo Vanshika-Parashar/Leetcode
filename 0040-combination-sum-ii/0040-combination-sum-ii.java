27001808f694a194cd69ca54f6cea40015fe9009
Vanshika Parashar <parasharvanshika2005@gmail.com>
2026-07-23 10:59:44 +0530

    Time: 6 ms (73.87%), Space: 45 MB (79.94%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.java
+++ 0040-combination-sum-ii/0040-combination-sum-ii.java
@@ -9,13 +9,16 @@ class Solution {
     public List<List<Integer>> find(int []arr, int t,int idx,List<List<Integer>>ans,List<Integer>l){
         if(t==0){
             ans.add(new ArrayList<>(l));
-            return ans;
         }
         for(int i=idx;i<arr.length;i++){
-            if(i>idx && arr[i]==arr[i-1])continue;
+            if(i>idx && arr[i]==arr[i-1]){
+                continue;
+            }
             if(t-arr[i]>=0){
+                t-=arr[i];
                 l.add(arr[i]);
-                find(arr,t-arr[i],i+1,ans,l);
+                find(arr,t,i+1,ans,l);
+                t+=arr[i];
                 l.remove(l.size()-1);
             }
         }
