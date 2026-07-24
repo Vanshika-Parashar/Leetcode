492dbf0851df43b902f1b117e0bb4648a3e1b5ee
Vanshika Parashar <parasharvanshika2005@gmail.com>
2026-07-24 20:36:43 +0530

    Time: 3 ms (30.32%), Space: 49.2 MB (10.42%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -1,21 +1,18 @@
 class Solution {
     public List<String> letterCombinations(String ques) {
-        List<String>ans=new ArrayList<>();
         String[]codes={"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+        List<String>ans=new ArrayList<>();
+        return find(ans,ques,codes,"");
+    }
+    public List<String> find(List<String> ans,String ques,String[]codes,String s){
         if(ques.length()==0){
+            ans.add(s);
             return ans;
         }
-        return letter(ques,codes,ans,"");
-    }
-    public List<String> letter(String ques,String[]codes,List<String> ans,String s){
-         if(ques.length()==0){
-                ans.add(s);
-                return ans;
-        }
         char ch=ques.charAt(0);
-        String press=codes[ch-48];
-        for(int i=0;i<press.length();i++){
-            letter(ques.substring(1),codes,ans,s+press.charAt(i));
+        String code=codes[ch-'0'];
+        for(int i=0;i<code.length();i++){
+            find(ans,ques.substring(1),codes,s+code.charAt(i));
         }
         return ans;
     }
