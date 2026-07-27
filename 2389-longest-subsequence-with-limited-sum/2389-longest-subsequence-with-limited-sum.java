4130f3d0b58f88ff148ad134302249e80e9933d3
Vanshika Parashar <parasharvanshika2005@gmail.com>
2026-07-27 18:37:43 +0530

    Time: 7 ms (100%), Space: 46.9 MB (85.44%) - LeetHub

--- 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.java
+++ 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.java
@@ -2,32 +2,33 @@ class Solution {
     public int[] answerQueries(int[] nums, int[] queries) {
         int n=nums.length;
         Arrays.sort(nums);
-        int prefix[]=new int[n];
+        int[]prefix=new int[n];
         prefix[0]=nums[0];
         for(int i=1;i<n;i++){
-            prefix[i]=prefix[i-1]+nums[i];
+            prefix[i]=nums[i]+prefix[i-1];
         }
-        int ans[]=new int[queries.length];
-        for(int i=0;i<queries.length;i++){
-            ans[i]=find(queries[i],prefix);
+        int m=queries.length;
+        int[]ans=new int[m];
+        for(int i=0;i<m;i++){
+            ans[i]=find(prefix,queries[i]);
         }
         return ans;
-        
     }
-    public int find(int query,int[]prefix){
+    public int find(int[]prefix,int ele){
         int st=0;
-        int end=prefix.length-1;;
-        int ans=-1;
+        int end=prefix.length-1;
+        
+        int max=-1;
         while(st<=end){
             int mid=st+(end-st)/2;
-            if(prefix[mid]<=query){
-                ans=Math.max(mid,ans);
-                st=mid+1;
-            }
-            else{
+            if(prefix[mid]>ele){
                 end=mid-1;
             }
+            else{
+                max=Math.max(mid,max);
+                st=mid+1;
+            }
         }
-        return ans+1;
+        return max+1;
     }
 }
