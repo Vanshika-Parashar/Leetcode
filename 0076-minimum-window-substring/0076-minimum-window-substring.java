ea923f6cc472286bff3d9d8eb1d36213b7bde94d
Vanshika Parashar <parasharvanshika2005@gmail.com>
2026-08-05 19:46:32 +0530

    Time: 16 ms (56.49%), Space: 47.3 MB (39.56%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,44 +1,42 @@
 class Solution {
     public String minWindow(String s, String t) {
-        HashMap<Character,Integer>map=new HashMap<>();
-        for(char ch:t.toCharArray()){
-            map.put(ch,map.getOrDefault(ch,0)+1);
-        }
-        int i=0;
-        int count=0;
-        int min=Integer.MAX_VALUE;
-        int start=-1;
-        for(int j=0;j<s.length();j++){
-            char ch=s.charAt(j);
-            if(map.containsKey(ch)){
-                if(map.get(ch)>0){
-                    count++;
-                    
-                    
-                }
-                map.put(ch,map.get(ch)-1);
-            }
-            while(count==t.length()){
-               if(j-i+1<min){
-                start=i;
-                min=j-i+1;
-               }
+    HashMap<Character,Integer>map=new HashMap<>();
+    int i=0;
+    for(i=0;i<t.length();i++){
+        char ch=t.charAt(i);
+        map.put(ch,map.getOrDefault(ch,0)+1);
+    }
+    i=0;
+    int min=Integer.MAX_VALUE;
+    int st=-1;
+    int required=t.length();
+    for(int j=0;j<s.length();j++){
+        char ch=s.charAt(j);
+        if(map.containsKey(ch)){
+           if(map.get(ch)>0){
+            required--;
+           }
+            map.put(ch,map.get(ch)-1);
             
-        
-                char c=s.charAt(i);
-                if(map.containsKey(c)){
-                    
-                
-                map.put(c,map.get(c)+1);
-                if(map.get(c)>0){
-                    count--;
-                }
-                }
-                i++;
-            }
         }
-        if(min==Integer.MAX_VALUE)return "";
-        return s.substring(start,start+min);
+        while(required==0){
+            if(j-i+1<min){
+                min=j-i+1;
+                st=i;
+            }
+            char c=s.charAt(i);
+            if(map.containsKey(c)){
+                int freq=map.get(c);
+                map.put(c,freq+1);
+                if(map.get(c)>0)required++;
+            }
+            i++;
 
+        }
+        
+        
+    }
+    if(min==Integer.MAX_VALUE)return "";
+    return (s.substring(st,st+min));
     }
 }
