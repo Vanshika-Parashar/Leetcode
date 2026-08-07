b42f073452fe710dc644489cf707912a646e3cac
Vanshika Parashar <parasharvanshika2005@gmail.com>
2026-08-08 00:59:04 +0530

    Time: 12 ms (9.26%), Space: 65.4 MB (41.36%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.java
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.java
@@ -1,32 +1,40 @@
 class Solution {
     public List<List<String>> partition(String s) {
-        List<String>l=new ArrayList<>();
-        List<List<String>>ans=new ArrayList<>();
-        return find(s,l,ans,0);
-        
+       List<String>list=new ArrayList<>();
+       List<List<String >>ans=new ArrayList<>();
+       return palindrome(s,0,list,ans);
     }
-    public List<List<String>>find(String s,List<String>l,List<List<String>>ans,int idx){
-        if(idx == s.length()) {
-            ans.add(new ArrayList<>(l));
+    public List<List<String >> palindrome(String s,int idx,List<String>list,List<List<String >>ans){
+        if(idx>=s.length()){
+            ans.add(new ArrayList<>(list));
             return ans;
         }
         for(int i=idx;i<s.length();i++){
-            if(palindrome(s,idx,i)){
-                l.add(s.substring(idx,i+1));
-                find(s,l,ans,i+1);
-                l.remove(l.size()-1);
+            String st=s.substring(idx,i+1);
+            if(ispalindrome(st)){
+                list.add(st);
+                palindrome(s,i+1,list,ans);
+                list.remove(list.size()-1);
             }
+            
+           
         }
-        return ans;
+         return ans;
     }
-    public boolean palindrome(String s,int st,int end){
-        int s1=st;
-        int e=end;
-        while(s1<e){
-            if(s.charAt(s1)!=s.charAt(e))return false;
-            s1++;
-            e--;
+    public boolean ispalindrome(String st){
+        int start=0;
+        int end=st.length()-1;
+        
+        char[]ch=st.toCharArray();
+        while(start<=end){
+            char temp=ch[start];
+            ch[start]=ch[end];
+            ch[end]=temp;
+            start++;
+            end--;
         }
-        return true;
+        String s=new String(ch);
+        return s.equals(st);
+
     }
 }
