78977fb51588d299304b5f04115c61827f6af581
Vanshika Parashar <parasharvanshika2005@gmail.com>
2026-09-03 17:33:50 +0530

    Time: 1447 ms (5.01%), Space: 47.9 MB (10.18%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
@@ -1,23 +1,27 @@
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
         char[]prr=new char[p.length()];
-        char []srr=new char[p.length()];
+        char[]srr=new char[p.length()];
         if(s.length()<p.length())return new ArrayList<>();
         for(int i=0;i<p.length();i++){
             prr[i]=p.charAt(i);
             srr[i]=s.charAt(i);
         }
+        List<Integer>ans=new ArrayList<>();
         Arrays.sort(prr);
         Arrays.sort(srr);
+        if(Arrays.equals(prr,srr)){
+            ans.add(0);
+        }
         int i=0;
-        List<Integer>ans=new ArrayList<>();
-        if(Arrays.equals(srr,prr))ans.add(0);
         for(int j=p.length();j<s.length();j++){
             i++;
             String st=s.substring(i,j+1);
-            char []ch=st.toCharArray();
+            char[]ch=st.toCharArray();
             Arrays.sort(ch);
-            if(Arrays.equals(prr,ch))ans.add(i);
+            if(Arrays.equals(ch,prr)){
+                ans.add(i);
+            }
             
         }
         return ans;
