069f8b88b20f8eb5c29ec3e8878301497580feb3
Vanshika Parashar <parasharvanshika2005@gmail.com>
2026-09-03 10:56:31 +0530

    Time: 30 ms (42.7%), Space: 124.6 MB (13.61%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
@@ -3,14 +3,14 @@ class Solution {
         Map<Integer,Integer>map=new HashMap<>();
         for(int i=0;i<nums.length;i++){
             if(map.containsKey(nums[i])){
-                int freqi=map.get(nums[i]);
-                int rem=Math.abs(freqi-i);
-                if(rem<=k)return true;
+                int rem=Math.abs(map.get(nums[i])-i);
+                if(rem<=k){
+                    return true;
+                }
             }
             map.put(nums[i],i);
         }
-           return false;
-
+        return false;
         
     }
 }
