c513c5482da1a13f984f5a7b2e16e21ad02826b1
Vanshika Parashar <parasharvanshika2005@gmail.com>
2026-09-03 18:14:37 +0530

    Time: 7 ms (68.1%), Space: 44.1 MB (47.74%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,22 +1,23 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        int []freq1=new int[26];
-        int[]freq2=new int[26];
-        if(s2.length()<s1.length())return false;
-        for(int i=0;i<s1.length();i++){
+       if(s2.length()<s1.length())return false;
+       int[]freq1=new int[26];
+       int[]freq2=new int[26];
+       for(int i=0;i<s1.length();i++){
+            char ch=s1.charAt(i);
             char c=s2.charAt(i);
-            char d=s1.charAt(i);
+            freq1[ch-'a']++;
             freq2[c-'a']++;
-            freq1[d-'a']++;
-        }
-        int i=0;
+       }
+       int i=0;
+       if(Arrays.equals(freq1,freq2))return true;
+       for(int j=s1.length();j<s2.length();j++){
+        freq2[s2.charAt(i)-'a']--;
+        i++;
+        freq2[s2.charAt(j)-'a']++;
         if(Arrays.equals(freq1,freq2))return true;
-        for(int j=s1.length();j<s2.length();j++){
-            freq2[s2.charAt(i)-'a']--;
-            i++;
-            freq2[s2.charAt(j)-'a']++;
-            if(Arrays.equals(freq2,freq1))return true;
-        }
-        return false;
+       }
+       return false;
+
     }
 }
