a72615fcb72cf7186a1f0e2113fa6910735a8f6d
Vanshika Parashar <parasharvanshika2005@gmail.com>
2026-09-05 02:16:22 +0530

    Time: 2 ms (61.87%), Space: 51.1 MB (33.76%) - LeetHub

--- 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.java
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.java
@@ -1,7 +1,7 @@
 class Solution {
 
-    public int numSubarraysWithSum(int[] nums, int k) {
-        if(k<=0){
+    public int numSubarraysWithSum(int[] nums, int goal) {
+        if(goal<=0){
             int ans=0;
             int len=0;
             for(int i:nums){
@@ -15,31 +15,29 @@ class Solution {
             }
             ans+=len*(len+1)/2;
             return ans;
-
         }
-        int a=0,i=0,j=0,k2=0,b=0,n=nums.length;
-        while(i<n && nums[i]!=1){
+        int i=0,j=0,a=0,b=0,n=nums.length,k=0,count=0;
+        while(i<n && nums[i]==0){
             i++;
         }
-        while(j<n && k2<k){
+        while(j<n && k<goal){
             if(nums[j]==1){
-                k2++;
+                k++;
             }
             j++;
         }
+        if(k<goal)return 0;
         j--;
-        if(k2<k)return 0;
         b=j+1;
-        while(b<n && nums[b]!=1){
+        while(b<n && nums[b]==0){
             b++;
         }
         b--;
-        int sum=0;
         while(b<n){
-            sum+=(i-a+1)*(b-j+1);
+            count+=(i-a+1)*(b-j+1);
             a=i+1;
             i++;
-            while(i<n && nums[i]!=1){
+            while(i<n && nums[i]==0){
                 i++;
             }
             j=b+1;
@@ -48,9 +46,9 @@ class Solution {
                 b++;
             }
             b--;
-           
+            
         }
-        return sum;
+        return count;
 
 
        
