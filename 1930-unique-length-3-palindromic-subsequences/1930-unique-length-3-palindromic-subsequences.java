beade43126f4722054204969a57d6cd5bc54ee9b
Vanshika Parashar <parasharvanshika2005@gmail.com>
2026-09-07 01:29:57 +0530

    Time: 233 ms (18.89%), Space: 47.2 MB (43.33%) - LeetHub

--- 1930-unique-length-3-palindromic-subsequences/1930-unique-length-3-palindromic-subsequences.java
+++ 1930-unique-length-3-palindromic-subsequences/1930-unique-length-3-palindromic-subsequences.java
@@ -1,27 +1,27 @@
 class Solution {
     public int countPalindromicSubsequence(String s) {
-        HashMap<Character,Integer>fmap=new HashMap<>();
-        HashMap<Character,Integer>lmap=new HashMap<>();
-        for(int i=0;i<s.length();i++){
-            char ch=s.charAt(i);
-            if(!fmap.containsKey(ch)){
-                fmap.put(ch,i);
-            }
-            lmap.put(ch,i);
+       HashMap<Character,Integer>fmap=new HashMap<>();
+       HashMap<Character,Integer>lmap=new HashMap<>();
+       for(int i=0;i<s.length();i++){
+        if(!fmap.containsKey(s.charAt(i))){
+            fmap.put(s.charAt(i),i);
         }
-        int count=0;
-        for(char ch:fmap.keySet()){
-            int foccur=fmap.get(ch);
-            int loccur=lmap.get(ch);
-            if(foccur!=loccur){
-                Set<Character>set=new HashSet<>();
-                for(int i=foccur+1;i<loccur;i++){
-                    set.add(s.charAt(i));
-                }
-                count+=set.size();
+        lmap.put(s.charAt(i),i);
+       }
+       int count=0;
+       for(char ch:fmap.keySet()){
+        int st=fmap.get(ch);
+        int end=lmap.get(ch);
+        if(st!=end){
+            Set<Character>set=new HashSet<>();
+            for(int i=st+1;i<end;i++){
+                set.add(s.charAt(i));
             }
+            count+=set.size();
         }
-        return count;
         
+        
+       }
+        return count;
     }
 }
