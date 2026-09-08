e8a300d71e1d0d8d15674c00899bdec62ae16477
Vanshika Parashar <parasharvanshika2005@gmail.com>
2026-09-08 10:57:48 +0530

    Time: 37 ms (10.28%), Space: 48.4 MB (5.28%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -9,17 +9,18 @@ class Solution {
         }
         long ans=find(coins,amount,0,dp);
         if(ans==Integer.MAX_VALUE)return -1;
-        return (int) ans;
+        int an=(int)(ans);
+        return an-1;
+        
     }
-    public long find(int[]coins,int amount,int i,long[][]dp){
-        if(i==coins.length){
-            if(amount==0)return 0;
-            else return Integer.MAX_VALUE;
-        }
-        if(dp[i][amount]!=-1)return dp[i][amount];
-        long skip=find(coins,amount,i+1,dp);
-        if(amount-coins[i]<0)return dp[i][amount]=  skip;
-        long pick=1+find(coins,amount-coins[i],i,dp);
-        return dp[i][amount]= Math.min(pick,skip);
+    public long find(int[]coins,int t,int i,long[][]dp){
+        if(t==0)return 1;
+        if(i==coins.length)return Integer.MAX_VALUE;
+        if(dp[i][t]!=-1)return dp[i][t];
+        long skip=find(coins,t,i+1,dp);
+        if(coins[i]>t)return dp[i][t]=skip;
+        long take=1+find(coins,t-coins[i],i,dp);
+        return dp[i][t]=Math.min(skip,take);
     }
+    
 }
