dd1b5f4f304659b36de6ada6dd04fd2764bdcdd3
Vanshika Parashar <parasharvanshika2005@gmail.com>
2026-09-18 23:15:55 +0530

    Time: 28 ms (19.63%), Space: 57.4 MB (17.06%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -10,12 +10,16 @@ class Solution {
         return find(coins,amount,0,dp);
     }
     public int find(int []coins,int t,int i,int[][]dp){
-        if(t==0)return 1;
-        if(i==coins.length)return 0;
+        if(i==coins.length){
+            if(t==0)return 1;
+            return 0;
+        }
         if(dp[i][t]!=-1)return dp[i][t];
         int skip=find(coins,t,i+1,dp);
-        if(coins[i]>t)return dp[i][t]=skip;
+        if(t<coins[i]){
+            return dp[i][t]=skip;
+        }
         int take=find(coins,t-coins[i],i,dp);
-        return dp[i][t]= take+skip;
+        return dp[i][t]=take+skip;
     }
 }
