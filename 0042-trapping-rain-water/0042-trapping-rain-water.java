f443ffb7f384e3f1a0c722e8fccc0e7733e5f03d
Vanshika Parashar <parasharvanshika2005@gmail.com>
2026-09-20 17:37:49 +0530

    Time: 0 ms (100%), Space: 47.1 MB (98.39%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -8,7 +8,7 @@ class Solution {
        int ans=0;
        while(l<r){
         lmax=Math.max(lmax,height[l]);
-        rmax=Math.max(height[r],rmax);
+        rmax=Math.max(rmax,height[r]);
         if(lmax<rmax){
             ans+=lmax-height[l];
             l++;
