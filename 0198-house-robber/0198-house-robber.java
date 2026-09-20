f4d48dc0e79d24b69942094a2b33fee154a8ef0b
Vanshika Parashar <parasharvanshika2005@gmail.com>
2026-09-20 17:21:49 +0530

    Time: 0 ms (100%), Space: 42.7 MB (74.99%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,6 +1,6 @@
 class Solution {
     public int rob(int[] nums) {
-     int []dp=new int[nums.length];
+     int[]dp=new int[nums.length];
      Arrays.fill(dp,-1);
      return find(nums,0,dp);
     }
