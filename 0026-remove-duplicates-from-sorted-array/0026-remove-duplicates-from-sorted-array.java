b59488d697ba06adea605c00e0ce05841fed8ef1
Vanshika Parashar <parasharvanshika2005@gmail.com>
2026-09-21 10:36:05 +0530

    Time: 4 ms (4.98%), Space: 46.8 MB (42.83%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -11,14 +11,24 @@ class Solution {
         // }
         // return k;
 
-        int j=1;
-        for(int i=1;i<nums.length;i++){
-            if(nums[i]!=nums[i-1]){
-                nums[j]=nums[i];
-                j++;
-            }
+        // int j=1;
+        // for(int i=1;i<nums.length;i++){
+        //     if(nums[i]!=nums[i-1]){
+        //         nums[j]=nums[i];
+        //         j++;
+        //     }
+        // }
+        // return j;
+        LinkedHashSet<Integer>l=new LinkedHashSet<>();
+        for(int i:nums){
+            l.add(i);
         }
-        return j;
+        int k=0;
+        for(int i:l){
+            nums[k]=i;
+            k++;
+        }
+        return k;
         
 
 
