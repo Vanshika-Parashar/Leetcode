e202cc9ec7621efbd2cd2ddde9ed118e81fa724b
Vanshika Parashar <parasharvanshika2005@gmail.com>
2026-09-21 23:48:19 +0530

    Time: 46 ms (32.02%), Space: 80.7 MB (95.02%) - LeetHub

--- 2064-minimized-maximum-of-products-distributed-to-any-store/2064-minimized-maximum-of-products-distributed-to-any-store.java
+++ 2064-minimized-maximum-of-products-distributed-to-any-store/2064-minimized-maximum-of-products-distributed-to-any-store.java
@@ -1,35 +1,33 @@
 class Solution {
-    public boolean ispossible(int max,int n,int[]arr){
-        int stores=0;
-        for(int i=0;i<arr.length;i++){
-            stores+=arr[i]/max;
-            if(arr[i]%max!=0){
-               stores+=1;
-            }
+    public int   minimizedMaximum(int n,int[]arr){
+        int st=1;
+        int end=Integer.MIN_VALUE;
+        for(int i:arr){
+            end=Math.max(i,end);
         }
-        if(stores>n){
-            return false;
-        }
-        else{
-            return true;
-        }
-    }
-    public int minimizedMaximum(int n, int[] arr) {
-        int lo=1;
-        int hi=100000;
         int ans=0;
-        while(lo<=hi){
-            int mid=lo+(hi-lo)/2;
-            if(ispossible(mid,n,arr)){
+        while(st<=end){
+            int mid=st+(end-st)/2;
+            int stores=find(arr,mid);
+            if(stores<=n){
                 ans=mid;
-                hi=mid-1;
-                
-            }
-            else{
-                lo=mid+1;
+                end=mid-1;
+            }else{
+                st=mid+1;
             }
         }
         return ans;
         
+    }
+    public int find(int[]arr, int mid){
+        int stores=0;
+        for(int i:arr){
+            if(i%mid==0){
+                stores+=i/mid;
+            }else{
+                stores+=i/mid+1;
+            }
+        }
+        return stores;
     }
 }
