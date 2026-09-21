5ecc035e85512f99ac94c06bbaf12bab1e1422fd
Vanshika Parashar <parasharvanshika2005@gmail.com>
2026-09-21 18:05:16 +0530

    Time: 1 ms (99.57%), Space: 43 MB (8.92%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -2,18 +2,19 @@ class Solution {
     public int mySqrt(int n) {
         int st=0;
         int end=n;
+        int ans=0;
         while(st<=end){
             int mid=st+(end-st)/2;
-            if((long)mid*mid==(long)n)return mid;
-        
-            else if((long)mid * mid>(long)n){
-            end=mid-1;
+            if((long)mid*mid==(long)n){
+                return mid;
             }
-            else{
-            st=mid+1;
+            else if((long)mid*mid>(long)n){
+                end=mid-1;
+            }else{
+                st=mid+1;
             }
         }
-        return Math.round(end);
+        return end;
         
     }
 }
