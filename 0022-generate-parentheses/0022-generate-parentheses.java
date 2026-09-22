fbfd636dc2a06e317c8731359cf71554c072e4e4
Vanshika Parashar <parasharvanshika2005@gmail.com>
2026-09-22 17:44:47 +0530

    Time: 3 ms (14.41%), Space: 44.9 MB (21.21%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.java
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -1,19 +1,20 @@
 class Solution {
     public List<String> generateParenthesis(int n) {
         List<String>ans=new ArrayList<>();
-        return find(n,ans,"",0,0);
+        return find(n,0,0,ans,"");
     }
-    public List<String> find(int n,List<String>ans,String s,int oc,int cc){
+    public List<String> find(int n ,int oc,int cc,List<String>ans,String s){
         if(oc==n && cc==n){
             ans.add(s);
             return ans;
         }
-        if(oc<n){
-            find(n,ans,s+'(',oc+1,cc);
+        if(oc< n){
+            find(n,oc+1,cc,ans,s+"(");
         }
-        if(cc<n && cc<oc){
-            find(n,ans,s+')',oc,cc+1);
+        if(cc<oc){
+            find(n,oc,cc+1,ans,s+")");
         }
         return ans;
     }
+    
 }
