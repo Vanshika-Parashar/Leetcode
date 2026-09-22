f96c05fe3c028d322fe2159fbeeb975618e4c65f
Vanshika Parashar <parasharvanshika2005@gmail.com>
2026-09-22 18:31:26 +0530

    Time: 1 ms (87.2%), Space: 44 MB (87.68%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -5,12 +5,12 @@ class Solution {
         return find(nums,0,ans,list);
     }
     public List<List<Integer>> find(int[]nums,int idx,List<List<Integer>>ans,List<Integer>list){
-        ans.add(new ArrayList<>(list));
-        for(int i=idx;i<nums.length;i++){
-            list.add(nums[i]);
-            find(nums,i+1,ans,list);
-            list.remove(list.size()-1);
-        }
-        return ans;
+       ans.add(new ArrayList<>(list));
+       for(int i=idx;i<nums.length;i++){
+        list.add(nums[i]);
+        find(nums,i+1,ans,list);
+        list.remove(list.size()-1);
+       }
+       return ans;
     }
 }
