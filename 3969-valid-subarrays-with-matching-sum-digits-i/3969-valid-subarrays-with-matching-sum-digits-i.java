1fa7375d53337587a137bd6e49d3e9e30068b094
Vanshika Parashar <parasharvanshika2005@gmail.com>
2026-09-22 22:08:33 +0530

    Time: 1200 ms (34.04%), Space: 47.2 MB (31.33%) - LeetHub

--- 3969-valid-subarrays-with-matching-sum-digits-i/3969-valid-subarrays-with-matching-sum-digits-i.java
+++ 3969-valid-subarrays-with-matching-sum-digits-i/3969-valid-subarrays-with-matching-sum-digits-i.java
@@ -1,25 +1,29 @@
 class Solution {
     public int countValidSubarrays(int[] nums, int x) {
-        int count=0;
-        for(int i=0;i<nums.length;i++){
-            long sum=0;
-            for(int j=i;j<nums.length;j++){
-                sum+=nums[j];
-                if(find(sum,x)){
-                    count++;
-                }
+       int count=0;
+       for(int i=0;i<nums.length;i++){
+        long sum=0;
+        for(int j=i;j<nums.length;j++){
+            sum+=nums[j];
+            if(isplaindrome(sum,x)){
+                count++;
             }
         }
-        return count;
+       }
+       return count;
     }
-    public boolean find(long sum,int x){
-        if(sum<10 && sum==x)return true;
-        int last=(int)(sum%10);
-        while(sum>=10){
-            sum/=10;
+    public boolean isplaindrome(long sum,int x){
+        
+        long last=sum%10;
+        
+        
+    
+        while( sum>=10){
+            sum=sum/10;
         }
-        int first=(int)sum;
-        if(first==x && last==x)return true;
+        long first=sum;
+        if(last==x && sum==x)return true;
         return false;
+
     }
 }
