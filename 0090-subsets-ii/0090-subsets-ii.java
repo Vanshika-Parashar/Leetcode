8650b6cf2a55d99a1ce492449ff8a2e03b49703a
Vanshika Parashar <parasharvanshika2005@gmail.com>
2026-09-22 20:44:37 +0530

    Time: 3 ms (53.71%), Space: 45.2 MB (23.76%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.java
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -8,9 +8,7 @@ class Solution {
     public List<List<Integer>> find(int[]nums,int idx,List<List<Integer>>ans,List<Integer>list){
         ans.add(new ArrayList<>(list));
         for(int i=idx;i<nums.length;i++){
-            if(i>idx && nums[i]==nums[i-1]){
-                continue;
-            }
+            if(i>idx && nums[i]==nums[i-1])continue;
             list.add(nums[i]);
             find(nums,i+1,ans,list);
             list.remove(list.size()-1);
