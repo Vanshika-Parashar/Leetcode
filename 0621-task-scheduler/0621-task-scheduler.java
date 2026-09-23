2b257a52bf8462855fa5acdf9b8891822fadafdf
Vanshika Parashar <parasharvanshika2005@gmail.com>
2026-09-23 22:24:42 +0530

    Time: 16 ms (55.41%), Space: 46.9 MB (99.78%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.java
+++ 0621-task-scheduler/0621-task-scheduler.java
@@ -4,18 +4,16 @@ class Solution {
         for(char ch:tasks){
             map.put(ch,map.getOrDefault(ch,0)+1);
         }
-        List<Map.Entry<Character,Integer>>list=new ArrayList<>(map.entrySet());
+        ArrayList<Map.Entry<Character,Integer>>list=new ArrayList<>(map.entrySet());
         Collections.sort(list,(a,b)->b.getValue()-a.getValue());
-        int maxfreq=list.get(0).getValue();
-        int idle=(maxfreq-1)*n;
+        int maxfreq=list.get(0).getValue()-1;
+        int idle=n*maxfreq;
         for(int i=1;i<list.size();i++){
             int val=list.get(i).getValue();
-            idle-=Math.min(maxfreq-1,val);
+            idle-=Math.min(maxfreq,val);
         }
-        if(idle<=0){
-            return tasks.length;
-        }
-        return tasks.length+idle;
+        if(idle<=0)return tasks.length;
+        return idle+tasks.length;
 
     }
 }
