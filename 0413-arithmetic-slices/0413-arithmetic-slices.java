b5b662f8ef11dad6b7d6ecf4d7b13c290b90ab05
Vanshika Parashar <parasharvanshika2005@gmail.com>
2026-09-24 17:56:32 +0530

    Time: 0 ms (100%), Space: 43.2 MB (42.7%) - LeetHub

--- 0413-arithmetic-slices/0413-arithmetic-slices.java
+++ 0413-arithmetic-slices/0413-arithmetic-slices.java
@@ -15,12 +15,13 @@ class Solution {
 
 
     //dp
-    int[]dp=new int[nums.length];
     int count=0;
+    int[]dp=new int[nums.length];
     for(int i=2;i<nums.length;i++){
         if(nums[i]-nums[i-1]==nums[i-1]-nums[i-2]){
             dp[i]=dp[i-1]+1;
             count+=dp[i];
+           
         }else{
             dp[i]=0;
         }
