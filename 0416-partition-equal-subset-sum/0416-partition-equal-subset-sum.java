50cfc7fcf832196ac2b09fd5b3ab8f2bc3c300de
Vanshika Parashar <parasharvanshika2005@gmail.com>
2026-09-26 13:31:42 +0530

    Time: 99 ms (16.93%), Space: 76.2 MB (11.72%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -1,33 +1,31 @@
 class Solution {
     public boolean canPartition(int[] nums) {
         int sum=0;
-        int n=nums.length;
-        
-        for(int i=0;i<nums.length;i++){
-            sum+=nums[i];
+        for(int i:nums){
+            sum+=i;
+
         }
         if(sum%2!=0)return false;
         int t=sum/2;
-        int[][]dp=new int[n][t+1];
-        for(int i=0;i<n;i++){
+        int[][]dp=new int[nums.length][t+1];
+        for(int i=0;i<nums.length;i++){
             for(int j=0;j<dp[0].length;j++){
                 dp[i][j]=-1;
             }
         }
-        int ans= find(nums,t,dp,0);
+        int ans= find(nums,0,t,dp);
         if(ans==1)return true;
         return false;
     }
-    public int find(int []nums,int t,int[][]dp,int i){
-       if(i==nums.length){
-        if(t==0)return 1;
-        return 0;
-       }
-       if(dp[i][t]!=-1)return dp[i][t];
-       int skip=find(nums,t,dp,i+1);
-
-       if(nums[i]>t)return dp[i][t]=skip;
-       int take= find(nums,t-nums[i],dp,i+1);
-       return dp[i][t]=Math.max(take,skip);
+    public int find(int []nums,int i,int t,int[][]dp){
+        if(i==nums.length){
+            if(t==0)return 1;
+            return 0;
+        }
+        if(dp[i][t]!=-1)return dp[i][t];
+        int skip=find(nums,i+1,t,dp);
+        if(t<nums[i])return dp[i][t]=skip;
+        int take=find(nums,i+1,t-nums[i],dp);
+        return dp[i][t]=Math.max(take,skip); 
     }
 }
