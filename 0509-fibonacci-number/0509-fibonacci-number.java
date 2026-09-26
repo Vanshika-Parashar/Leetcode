56b99424089273a95abde027371e1bdfb1583599
Vanshika Parashar <parasharvanshika2005@gmail.com>
2026-09-26 13:05:06 +0530

    Time: 0 ms (100%), Space: 41.9 MB (85.06%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -2,12 +2,12 @@ class Solution {
     public int fib(int n) {
        int[]dp=new int[n+1];
        Arrays.fill(dp,-1);
-       return find(n,dp);
+       return find(dp,n);
     }
-    public int find(int n,int[]dp){
-        if(n<=1)return n;
+    public int find(int[]dp,int n){
+        if(n<=1)return n ;
         if(dp[n]!=-1)return dp[n];
-        return dp[n]=find(n-1,dp)+find(n-2,dp);
+        return dp[n]=find(dp,n-1)+find(dp,n-2);
     }
     
 }
